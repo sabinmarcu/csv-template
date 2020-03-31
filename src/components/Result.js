f61530392ff332956611b7939223b98f155cc4c7
Sabin Marcu <sabin.marcu@rga.com>
2020-03-31 15:43:35 +0300

    fix: added clipboard copy

--- src/components/Result.js
+++ src/components/Result.js
@@ -6,19 +6,21 @@ import {
   Input,
 } from '@material-ui/core';
 
+import useClipboard from 'react-use-clipboard';
 import { ResultContext } from '../hooks/useResult';
 
 export const ResultComponent = () => {
   const result = useContext(ResultContext);
+  const [isCopied, setCopied] = useClipboard(result, { successDuration: 1000 });
   return (
     <Card>
-      <CardHeader title={'Template'} />
-      <CardContent>
+      <CardHeader title={`Template ${isCopied ? `(Copied!)` : ''}`} />
+      <CardContent onClick={setCopied}>
         <Input 
           fullWidth
-          disabled
           multiline
           value={result}
+          onClick={setCopied}
         />
       </CardContent>
     </Card>
